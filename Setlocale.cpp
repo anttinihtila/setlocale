6edbc72655f1bd377ca81528c8aaa1fd62e7ea95
Antti Nihtilä <68847740+anttinihtila@users.noreply.github.com>
2024-04-17 09:42:45 +0300

    Add files via upload

---
+++ Setlocale.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   setlocale(LC_ALL, "sv-FI");
+   cout << "Tämä ohjelma testaa toimivatko ääkköset" << endl;
+   cout << "Ä kirjain" << endl;
+   cout << "Ö kirjain" << endl;
+   return 0;
+}
+
